99eba58a546dfab15e2e6cb5998b271e5d59c76e
illei <illei@DESKTOP-UNIQHU1>
2023-09-06 10:21:02 +0200

    Add project files.

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inaltime;
+            int lungime;
+            int latime;
+            int volum;
+            /*Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+                paralelipiped dreptunghic, va afisa volumul lui*/
+            Console.WriteLine("Introduceti inaltimea Paralepipedului:");
+            inaltime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti lungimea Paralepipedului:");
+            lungime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti latimea Paralepipedului:");
+            latime = int.Parse(Console.ReadLine());
+            volum = inaltime * lungime * latime;
+
+
+            Console.WriteLine("Volumul paralepipedului este :" + volum);
+        }
+    }
+}
